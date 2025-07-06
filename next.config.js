8ec15737ffab7bfd39900a20a11e52007260e6c0
Terry-Mathew <terry.perangat@gmail.com>
2025-07-06 20:06:45 +0530

    Fix Next.js config: remove deprecated appDir option and add Supabase image domain

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['supabase.co'],
+    domains: ['supabase.co', 'azdejrutqthkgacaipvw.supabase.co'],
   },
 }
 
